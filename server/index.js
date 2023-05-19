560165ba560c7ef06c785347dc8e780ae837cf6f
Abdussamet <smt.3000.dgn@gmail.com>
2023-05-20 01:29:21 +0300

    s

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/", Auth);
 app.use("/", Post);
-app.get("/",(req,res)=>{
+app.get("/test",(req,res)=>{
   res.status(200).json({ message:"ula ula ula" });
 })
 
